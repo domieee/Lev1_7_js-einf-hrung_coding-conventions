d73e605ad8f3dc9b123c10550fbc312d9944c600
domieee <99103235+domieee@users.noreply.github.com>
2023-01-03 13:40:48 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let helloworld = 'Hello World';
+const pi = 3.14;
+const burjdubai = 828; 
+const fullname = 'Jan Schmidt'; 
+const eifeltower = 324;
+let camelcase = 'camelCase';
+let kursstatus = true;
